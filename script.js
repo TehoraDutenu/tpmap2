cebc2ea06afa93959340095435941608564638e2
TehoraDutenu <meenahbass@hotmail.com>
2023-06-16 17:06:28 +0200

    final

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // ajout des contrôles de nav
   map.addControl(new mapboxgl.NavigationControl(), "top-left");
 
-  // Fonction pour charger les événements à partir du localStorage
+  // FONCTION pour charger les événements à partir du localStorage
   function loadEventDataFromLocalStorage() {
     let savedEventData = localStorage.getItem("eventData");
     if (savedEventData) {
@@ -27,10 +27,10 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-  // Appeler la fonction lors du chargement de la page
+  // Appeler la fonction de restitution du localStorage lors du chargement de la page
   window.addEventListener("load", loadEventDataFromLocalStorage);
 
-  // Fonction pour ajouter un marqueur avec une popup associée
+  // FONCTION pour ajouter les markers colorés avec popups
   function addMarkerWithPopup(eventData) {
     let markerDiv = document.createElement("div");
     markerDiv.style.width = "25px";
@@ -39,47 +39,40 @@ document.addEventListener("DOMContentLoaded", function() {
     markerDiv.style.borderRadius = "50%";
     markerDiv.style.cursor = "pointer";
 
-    // colorer les markers en fonction du delai
-    // la date du jour de consultation
-    let currentDate = Date.now();
-
-    // récupérer la date de l'event
-    let eventDate = new Date(eventData.start).getTime();
-
-    // calculer la différence
-    let timeDiff = eventDate - currentDate;
-
+    // créer et colorer les markers en fonction des coordonnées géo et du delai
+    let currentDate = Date.now(); // la date du jour de consultation
+    let eventDate = new Date(eventData.start).getTime(); // récupérer la date de l'event
+    let timeDiff = eventDate - currentDate; // calculer la différence
     // calculer le nombre de jours entre la date du jour de consultation et la date de l'event
-    let daysDiff = Math.floor(timeDiff / (1000 * 3600 *24));
-
+    let daysDiff = Math.floor(timeDiff / (1000 * 3600 *24)); 
+    
     // définir la couleur du marker
     let color;
     if(daysDiff > 3) {
-      // pour un event dans plus de trois jours
-      color = 'green';
+      color = 'green'; // pour un event dans plus de trois jours
     } else if (daysDiff <= 3 && daysDiff >= 0) {
-      // pour un event dans trois jours ou moins
-      color = 'orange';
+      color = 'orange'; // pour un event dans trois jours ou moins
     } else {
-      // event passé
-      color = 'red';
+      color = 'red'; // event passé
     }
-
     
     let marker = new mapboxgl.Marker({ color: color }).setLngLat([eventData.longitude, eventData.latitude]).addTo(map);
     
+    // améliorer le rendu des dates et horaires
     let options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
     
+    // préparer la popup au passage de souris
     let hoverPopupContent = "<h4>" + eventData.title + "</h4>";
     hoverPopupContent += "<p>Date de début : " + new Date(eventData.start).toLocaleString('fr-FR', options) + "</p>";
     
     let hoverPopup = new mapboxgl.Popup({ closeOnClick: false, closeButton: false}).setHTML(hoverPopupContent).setMaxWidth("400px");
     
+    // préparer la popup au click
     let clickPopupContent = "<h4>" + eventData.title + "</h4>";
     clickPopupContent += "<p>" + eventData.description + "</p>";
     clickPopupContent += "<p>Date de début : " + new Date(eventData.start).toLocaleString('fr-FR', options) + "</p>";
     clickPopupContent += "<p>Date de fin : " + new Date(eventData.end).toLocaleString('fr-FR', options) + "</p>";
-
+    
     let clickPopup = new mapboxgl.Popup({ closeOnClick: false, closeButton: false }).setHTML(clickPopupContent).setMaxWidth("400px");
     
     // ajouter des messages dans les popup de markers orange et rouge
@@ -90,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function() {
       clickPopup.setHTML(clickPopupContent + "<p>Quel dommage ! Vous avez raté cet événement !");
     }
     
-
+    // écouter les click et les hover
     marker.getElement().addEventListener('mouseenter', function () {
       marker.setPopup(hoverPopup).togglePopup();
     });
@@ -103,6 +96,7 @@ document.addEventListener("DOMContentLoaded", function() {
       marker.setPopup(clickPopup).togglePopup();
     });
 
+    // afficher le popup associé au click
     marker.setPopup(clickPopup);
   }
 
